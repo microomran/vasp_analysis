551d54d62240051fd2c89bf15aebd1d7eb8603a0
qzhang62 <jerryzhang0130@gmail.com>
2016-12-23 17:36:57 -0500

    38restart.sh 48restart.sh NELM_reach_solve.sh: resolve job runs with errors

---
+++ 48restart.sh
@@ -0,0 +1,7 @@
+qclear.sh 1
+editTag.sh modify ISTART 0
+editTag.sh modify ICHARG 2
+editTag.sh modify EDIFF 1e-3
+editTag.sh modify NSW 20
+editTag.sh modify IALGO 48
+IBRION2.sh
---
+++ NELM_reach_solve.sh
@@ -0,0 +1,21 @@
+if [ $# -eq 0 ];then
+   echo -e "please specify job list file"
+   echo -e "Use ~/luo/Oper.sh"
+   exit
+fi
+filename=$1
+IFS=$'\n'
+i=1
+for next in `cat $filename`
+do
+   if [ -d $next ];then
+     cd $next
+     PATH=$PATH:~/luo/
+     38restart.sh
+     qsub runvasp.pbs
+   fi
+   i=$((i+1))
+done
+echo "success"
+exit 0
+
